2b154da369914377ae174354110ba8e5667c51fc
Peso4ek-program <timur.sibgatov023@mail.ru>
2021-02-26 21:57:43 +0300

    1week_3task

---
+++ 1week_3task/1week_3task/1week_3task.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+#define PI 3.14159265
+
+
+/*
+Пользователь вводит число. Выведите на экран квадрат этого числа, куб этого
+числа., Вариант 5
+*/
+int main()
+{
+	float a;
+	float kv;
+	float ku;
+
+	std::cout << "Vvedite chislo: ";
+	std::cin >> a;
+	kv = a * a;
+	ku = a * a * a;
+
+	std::cout << "kvadrat = " << kv << std::endl
+		<< "Kub = " << ku << std::endl;
+	return 0;
+}
