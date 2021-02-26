cd6aaa8b983d6b3527e3bb6fdcd752e96e0eef21
Peso4ek-program <timur.sibgatov023@mail.ru>
2021-02-26 21:47:57 +0300

    1task_1week

---
+++ 1week_1task/1week_1task/1week_1task.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main()
+{
+    std::cout << "********" << std::endl
+               << "   *   " << std::endl
+               << "   *   " << std::endl
+               << "   *   " << std::endl
+               << "   *   " << std::endl
+               << "   *   " << std::endl
+               << "   *   " << std::endl;
+
+    std::getchar(); // ожидание ввода, чтобы прога
+    std::getchar(); // не закрывалась после выполнения
+
+    return 0;
+}
