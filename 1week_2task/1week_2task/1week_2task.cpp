61ee5cc43ccf0c3791973c9f884f495e78918978
Peso4ek-program <timur.sibgatov023@mail.ru>
2021-02-26 21:53:55 +0300

    1week_2task

---
+++ 1week_2task/1week_2task/1week_2task.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip> // чтобы работал setprecision
+#include <cmath>
+#define PI 3.14159265
+
+
+/*
+ Ромб, площадь и периметр, Вариант 5
+*/
+int main()
+{
+    float a;
+    float b;
+    float S;
+    float P;
+
+    std::cout << "Vvedite storonu and odin ugol romba: ";
+    std::cin >> a >> b;
+    P = a * 4;
+    if (b < 180 && b >= 0);
+    S = (a * a * sin(b * PI / 180));
+
+    std::setprecision(7);
+    std::cout << "Perimetr = " << P << std::endl
+        << "Ploshad = " << S << std::endl;
+    return 0;
+}
